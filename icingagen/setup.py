9d02a2dbe8a31c437dae6ac9529aa66ea3ec7e53
Tim Stallard <code@timstallard.me.uk>
2019-11-18 04:14:14 +0000

    attempt at restructuring into package

---
+++ icingagen/setup.py
@@ -0,0 +1,34 @@
+"""icingagen package configuration."""
+from setuptools import setup
+
+setup(
+    name="icingagen",
+    version="0.1",
+    description="SOWN Icinga 2 monitoring configuration management",
+    url="https://github.com/sown/monitoring",
+    author="SOWN",
+    packages=["icingagen"],
+    zip_safe=False,
+    install_requires=[
+        "pynetbox",
+        "jinja2",
+        "requests",
+    ],
+    extras_require={
+        "dev": [
+            "flake8",
+            "flake8-commas",
+            "flake8-comprehensions",
+            "flake8-debugger",
+            "flake8-mutable",
+            "flake8-todo",
+            "flake8-docstrings",
+            "flake8-isort",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "icingagen=icingagen.__main__:main",
+        ],
+    },
+)
