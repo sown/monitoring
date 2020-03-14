a1eb7c5bd2f63edb3e4a1955ef9391048cced48a
Tim Stallard <code@timstallard.me.uk>
2020-03-14 14:34:04 +0000

    set colours for states, include state changes

--- scripts/irc-notification.sh
+++ scripts/irc-notification.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 state="${SERVICESTATE:-$HOSTSTATE}";
+laststate="${LASTSERVICESTATE:-$LASTHOSTSTATE}";
 
+colourstate () {
+	# default to red
+	statecolour=04
+	if [ "$1" == "UP" ] || [ "$1" == "OK" ]
+	then
+		statecolour="03"
+	fi
+	if [ "$1" == "WARNING" ]
+	then
+		statecolour="08"
+	fi
+	echo "\x03$statecolour$1\x03"
+}
+
+colouredstate=$(colourstate $state)
+colouredlaststate=$(colourstate $laststate)
+
+# default to red
 color="04"
-if [ "$state" == "UP" ] || [ "$state" == "OK" ]
-then
-	color="03"
-fi
 if [ "$NOTIFICATIONTYPE" == "ACKNOWLEDGEMENT" ] || [ "$NOTIFICATIONTYPE" == "CUSTOM" ]
 then
 	color="02"
 fi
 
-colouredstate="\x03$color$state\x03"
+if [ "$NOTIFICATIONTYPE" == "RECOVERY" ]
+then
+	color="03"
+fi
+
 colouredtype="\x03$color$NOTIFICATIONTYPE\x03"
 
 prefix=""
 middle=""
 if [ "$NOTIFICATIONUMBER" -gt 1 ] && [ "$NOTIFICATIONTYPE" != "ACKNOWLEDGEMENT" ]
 then
-	prefix="Reminder of "
-	middle="still "
-	suffix=" ($NOTIFICATIONUMBER reminders since $LONGDATETIME)"
+	if [ "$laststate" == "$state" ]
+	then
+		prefix="Reminder of "
+		middle="still "
+		suffix=" ($NOTIFICATIONUMBER reminders since $LONGDATETIME)"
+	else
+		middle="State change $colouredlaststate -> "
+	fi
 fi
 
 if [ -z "$SERVICESTATE" ]
 then
-	alert="$HOSTNAME is"
+	alert="$HOSTNAME"
 else
 	alert="$HOSTNAME/$SERVICENAME"
 fi
